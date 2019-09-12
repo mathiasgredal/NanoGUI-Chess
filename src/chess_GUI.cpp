ee94df2e42facb6b9cb42c4c999292cd743da1a1
Mathias Gredal <Mathiasgredal@icloud.com>
2019-09-12 21:45:35 +0200

    Cmake now works on windows

--- src/chess_GUI.cpp
+++ src/chess_GUI.cpp
@@ -1,18 +1,18 @@
 #include <vector>
-#include <FL/Fl.H>
-#include <FL/fl_draw.H>
-#include <FL/Fl_Double_Window.H>
+#include "FL/Fl.H"
+#include "FL/fl_draw.H"
+#include "FL/Fl_Double_Window.H"
 #include "chess_gui.h"
 #include <iostream>
 
-chess_GUI::chess_GUI(int W, std::vector<std::vector<int> > startposition) : Fl_Widget(0,0,W,W,0), board_size(W), s_size(W/10)
+chess_GUI::chess_GUI(int W, std::vector<std::vector<int>> startposition) : Fl_Widget(0, 0, W, W, 0), board_size(W), s_size(W / 10)
 {
     board = startposition;
-    click1 = {-1,-1};
-    click2 = {-1,-1};
+    click1 = {-1, -1};
+    click2 = {-1, -1};
 
     // If computer is white, start with a computer move
-    if(COM == WHITE)
+    if (COM == WHITE)
     {
         update_piece(calc_com_move(board));
     }
@@ -22,44 +22,44 @@ chess_GUI::chess_GUI(int W, std::vector<std::vector<int> > startposition) : Fl_W
 void chess_GUI::draw()
 {
     // After first click, mark it in upper left corner
-    if(click1[0]>=0)
+    if (click1[0] >= 0)
     {
-        fl_rectf(s_size*0.25,s_size*0.25,s_size*0.5,s_size*0.5,FL_GREEN);
+        fl_rectf(s_size * 0.25, s_size * 0.25, s_size * 0.5, s_size * 0.5, FL_GREEN);
         return;
     }
     else
     {
-        fl_rectf(s_size*0.25,s_size*0.25,s_size*0.5,s_size*0.5,WIN_COLOR);
+        fl_rectf(s_size * 0.25, s_size * 0.25, s_size * 0.5, s_size * 0.5, WIN_COLOR);
     }
 
     // Draw numbers and letters outside board
     fl_color(FL_BLACK);
-    fl_font(FL_HELVETICA,s_size*0.75);
-    char row[2] = {'8','\0'};
-    char col[2] = {'A','\0'};
-    for(int r=1; r<9; r++)
+    fl_font(FL_HELVETICA, s_size * 0.75);
+    char row[2] = {'8', '\0'};
+    char col[2] = {'A', '\0'};
+    for (int r = 1; r < 9; r++)
     {
-        fl_draw(row,0,s_size*r, s_size, s_size, FL_ALIGN_CENTER);
-        fl_draw(row,board_size-s_size,s_size*r, s_size, s_size, FL_ALIGN_CENTER);
-        fl_draw(col,s_size*r,0, s_size, s_size, FL_ALIGN_CENTER);
-        fl_draw(col,s_size*r,board_size-s_size, s_size, s_size, FL_ALIGN_CENTER);
-        row[0] = row[0]-1;
-        col[0] = col[0]+1;
+        fl_draw(row, 0, s_size * r, s_size, s_size, FL_ALIGN_CENTER);
+        fl_draw(row, board_size - s_size, s_size * r, s_size, s_size, FL_ALIGN_CENTER);
+        fl_draw(col, s_size * r, 0, s_size, s_size, FL_ALIGN_CENTER);
+        fl_draw(col, s_size * r, board_size - s_size, s_size, s_size, FL_ALIGN_CENTER);
+        row[0] = row[0] - 1;
+        col[0] = col[0] + 1;
     }
 
     // Draw squares
     bool white = true;
-    for(int r=1; r<9; r++)
+    for (int r = 1; r < 9; r++)
     {
-        for(int c=1; c<9; c++)
+        for (int c = 1; c < 9; c++)
         {
-            if(white)
+            if (white)
             {
-                fl_rectf(s_size*c,s_size*r,s_size,s_size,FL_WHITE);
+                fl_rectf(s_size * c, s_size * r, s_size, s_size, FL_WHITE);
             }
             else
             {
-                fl_rectf(s_size*c,s_size*r,s_size,s_size,FL_GRAY);
+                fl_rectf(s_size * c, s_size * r, s_size, s_size, FL_GRAY);
             }
             white = !white;
         }
@@ -68,26 +68,25 @@ void chess_GUI::draw()
 
     // Draw pieces
     fl_color(FL_BLACK);
-    for(int r=0; r<8; r++)
+    for (int r = 0; r < 8; r++)
     {
-        for(int c=0; c<8; c++)
+        for (int c = 0; c < 8; c++)
         {
-            fl_draw(int_to_unicode(board[r][c]),s_size*(c+1),s_size*(r+1),s_size,s_size, FL_ALIGN_CENTER);
+            fl_draw(int_to_unicode(board[r][c]), s_size * (c + 1), s_size * (r + 1), s_size, s_size, FL_ALIGN_CENTER);
         }
     }
-
 }
 
 // Handle events (e.g. mouse clicking)
 int chess_GUI::handle(int event)
 {
     // After 2 clicks move the piece
-    if ( event == FL_PUSH )
+    if (event == FL_PUSH)
     {
-        std::vector<int> click = {Fl::event_y()/s_size-1,Fl::event_x()/s_size-1};
-        if( click[0] >= 0 && click[0] < 8 && click[1] >= 0 && click[1] < 8)
+        std::vector<int> click = {Fl::event_y() / s_size - 1, Fl::event_x() / s_size - 1};
+        if (click[0] >= 0 && click[0] < 8 && click[1] >= 0 && click[1] < 8)
         {
-            if(click1[0] < 0  && board[click[0]][click[1]] != EMPTY)
+            if (click1[0] < 0 && board[click[0]][click[1]] != EMPTY)
             {
                 click1 = click;
                 redraw();
@@ -96,14 +95,14 @@ int chess_GUI::handle(int event)
             {
                 click2 = click;
                 std::vector<int> tmp = click1;
-                tmp.insert(tmp.end(),click2.begin(),click2.end());
+                tmp.insert(tmp.end(), click2.begin(), click2.end());
                 update_piece(tmp); // Player move
-                click1 = {-1,-1};
-                click2 = {-1,-1};
+                click1 = {-1, -1};
+                click2 = {-1, -1};
                 redraw();
                 tmp = calc_com_move(board);
-                update_piece(tmp);  // Computer move
-                print_move(tmp);    // Print to terminal
+                update_piece(tmp); // Computer move
+                print_move(tmp);   // Print to terminal
                 redraw();
             }
         }
@@ -120,7 +119,7 @@ void chess_GUI::update_piece(std::vector<int> mv)
 }
 
 // Translate piece int value to corresponding unicode character
-const char* chess_GUI::int_to_unicode(int i)
+const char *chess_GUI::int_to_unicode(int i)
 {
     switch (i)
     {
